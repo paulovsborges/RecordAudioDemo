9a410061a56996d879eeba4c08ee2e6ccc8870d8
paulo borges <pauloborges@paulos-MacBook-Pro.local>
2023-07-09 20:52:11 -0300

    Initial commit

---
+++ app/src/main/java/com/pvsb/recordaudiodemo/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.pvsb.recordaudiodemo
+
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.drawable.TransitionDrawable
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LockerState {
+        LOCKED,
+        UNLOCKED
+    }
+
+    private var lockerState: LockerState = LockerState.UNLOCKED
+        set(value) {
+            field = value
+            handleNewLockerState(value)
+        }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val locker = findViewById<ConstraintLayout>(R.id.clLockRecorder)
+
+        locker.setOnTouchListener { _, motionEvent ->
+
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+
+                if (lockerState == LockerState.LOCKED) {
+                    lockerState = LockerState.UNLOCKED
+                }
+            }
+
+            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+
+                if ((motionEvent.y / 100) < 1.0) {
+                    lockerState = LockerState.LOCKED
+                }
+            }
+            true
+        }
+    }
+
+    private fun handleNewLockerState(newState: LockerState) {
+
+        val locker = findViewById<ConstraintLayout>(R.id.clLockRecorder)
+        val lockerIcon = findViewById<ImageView>(R.id.ivLocker)
+        val lockerArrow = findViewById<ImageView>(R.id.ivLockerArrow)
+
+        val applyAnim: (Boolean) -> Unit = fun(isLocked: Boolean) {
+
+            val animDuration = if (isLocked) {
+                250
+            } else {
+                100
+            }
+
+            val height = if (isLocked) {
+                40
+            } else {
+                56
+            }
+
+            (locker.background as TransitionDrawable).apply {
+                if (isLocked) {
+                    startTransition(animDuration)
+                } else {
+                    reverseTransition(animDuration)
+                }
+            }
+
+            ValueAnimator.ofInt(locker.height, height.toPx).apply {
+                addUpdateListener {
+                    val newValue = it.animatedValue as Int
+                    val params = locker.layoutParams
+                    params.height = newValue
+                    locker.layoutParams = params
+                }
+
+                duration = animDuration.toLong()
+                start()
+            }
+        }
+
+        when (newState) {
+            LockerState.LOCKED -> {
+                lockerIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_locked))
+                lockerArrow.visibility = View.GONE
+                applyAnim(true)
+            }
+
+            LockerState.UNLOCKED -> {
+                lockerIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_unlocked))
+                lockerArrow.visibility = View.VISIBLE
+                applyAnim(false)
+            }
+        }
+    }
+}
+
+val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
